9f635ca67d9a953cd257951a5725281ddb8785a2
FilipSzweda <filip.szweda1@gmail.com>
2022-03-19 11:04:03 +0100

    MACD and Signal plotting

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import pandas
-import pandas as pd
+import numpy
+from matplotlib import pyplot
 
 
-def ema(wig20, i, N):
-    alfa = 2 / (N + 1)
-    denominator = (1 - pow(1 - alfa, N)) / alfa
+def ema(wig20, i, n):
+    alfa = 2 / (n + 1)
+    denominator = (1 - pow(1 - alfa, n)) / alfa
     nominator = 0
-    power = N
-    if i - N >= 0:
-        for sample in range(i - N, i):
-            nominator += wig20.at[sample, 'Najwyzszy'] * pow(1 - alfa, power)
-            power -= 1
+    power = 0
+
+    if i - n < 0:
+        bottomRange = 0
     else:
-        for sample in range(0, i):
-            nominator += wig20.at[sample, 'Najwyzszy'] * pow(1 - alfa, power)
-            power -= 1
+        bottomRange = i - n
+
+    for sample in range(i + 1, bottomRange, -1):
+        if type(wig20) is pandas.DataFrame:
+            value = wig20.at[sample - 1, 'Najwyzszy']
+        else:
+            value = wig20[sample - 1]
+        nominator += value * pow(1 - alfa, power)
+        power += 1
+
     return nominator / denominator
 
 
 def main():
     wig20 = pandas.read_csv('wig20_d.csv', usecols=['Najwyzszy'])
-    ema9 = []
+
     ema12 = []
     ema26 = []
+    macd = []
+    signal = []
+
     for i in range(1000):
-        ema9.append(ema(wig20, i, 9))
         ema12.append(ema(wig20, i, 12))
         ema26.append(ema(wig20, i, 26))
+        macd.append(ema12[i] - ema26[i])
+        signal.append(ema(macd, i, 9))
+
+    pyplot.plot(list(range(1000)), macd)
+    pyplot.plot(list(range(1000)), signal)
+    pyplot.show()
 
 
 if __name__ == '__main__':

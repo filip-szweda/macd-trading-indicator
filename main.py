be559c8a57848725a2d61c943855a2af090f55eb
FilipSzweda <filip.szweda1@gmail.com>
2022-03-22 15:52:31 +0100

    Changed how EMA is calculated

--- main.py
+++ main.py
@@ -2,41 +2,17 @@ import pandas
 from matplotlib import pyplot
 
 
-def ema(data, current_day, period):
-    alfa = 2 / (period + 1)
-    denominator = (1 - pow(1 - alfa, period)) / alfa
-
-    nominator = 0
-    power = 0
-    last_day = current_day - period - 1
-
-    for sample in range(current_day, last_day, -1):
-        if type(data) is pandas.DataFrame:
-            value = data.at[sample, 'Otwarcie'] # later change to 'Zamkniecie'
-        else:
-            value = data[sample - 1]
-        nominator += value * pow(1 - alfa, power)
-        power += 1
-
-    return nominator / denominator
-
-
 def main():
-    data = pandas.read_csv('wig20_d.csv', usecols=['Otwarcie']) # later change to 'Zamkniecie'
+    data = pandas.read_csv('wig20_d.csv', usecols=['Otwarcie'])  # later change to 'Zamkniecie'
 
-    macd = []
-    signal = []
+    ema12 = data.ewm(span=12).mean()
+    ema26 = data.ewm(span=26).mean()
+    macd = ema12 - ema26
+    signal = macd.ewm(span=9).mean()
 
-    for sample_number in range(26, 1000):
-        ema12 = ema(data, sample_number, 12)
-        ema26 = ema(data, sample_number, 26)
-        macd.append(ema12 - ema26)
-        if len(macd) >= 9:
-            signal.append(ema(macd, len(macd), 9))
 
-    new_macd = macd[9:]
-    pyplot.plot(list(range(len(new_macd))), new_macd, label='MACD')
-    pyplot.plot(list(range(len(signal))), signal, label='Signal')
+    pyplot.plot(macd, label='MACD')
+    pyplot.plot(signal, label='Signal')
     pyplot.xlabel('Sample number')
     pyplot.ylabel('Opening')
     pyplot.title('MACD trading indicator')

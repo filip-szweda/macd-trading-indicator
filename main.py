aebc025a5f90600a2f4fefe205c64479bed42442
FilipSzweda <filip.szweda1@gmail.com>
2022-03-25 20:28:01 +0100

    Added Buy/Sell points

--- main.py
+++ main.py
@@ -33,16 +33,30 @@ def main():
     for sample_number in range(9, len(macd)):
         signal.append(ema(macd, sample_number, 9))
 
+    data = data[35:]
+    macd = macd[9:]
+    buy = []
+    sell = []
+    for i in range(len(macd)):
+        if i + 1 < len(macd):
+            if signal[i] > macd[i] and signal[i + 1] < macd[i + 1]:
+                buy.append((i, data[i]))
+            elif signal[i] < macd[i] and signal[i + 1] > macd[i + 1]:
+                sell.append((i, data[i]))
+
     fig, (ax1, ax2) = pyplot.subplots(2)
 
-    ax1.plot(data[35:])
-    ax1.set_ylabel('Opening stock quotes')
-    ax1.set_title('Input data and MACD trading indicator')
+    ax1.plot(macd, label='MACD')
+    ax1.plot(signal, label='Signal')
+    ax1.set_xlabel('Sample number')
+    ax1.set_ylabel('Indicator')
+    ax1.legend()
 
-    ax2.plot(macd[9:], label='MACD')
-    ax2.plot(signal, label='Signal')
-    ax2.set_xlabel('Sample number')
-    ax2.set_ylabel('Indicator')
+    ax2.plot(data)
+    ax2.scatter(*zip(*buy), c='g', label='Buy')
+    ax2.scatter(*zip(*sell), c='r', label='Sell')
+    ax2.set_ylabel('Opening stock quotes')
+    ax2.set_title('Input data and MACD trading indicator')
     ax2.legend()
 
     pyplot.show()

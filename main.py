4fbad77a706cd8ace5e9259eba084229ddbfc762
FilipSzweda <filip.szweda1@gmail.com>
2022-03-25 19:43:05 +0100

    Fixed calculating EMAx, now it properly takes x+1 samples

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot
 
 def ema(data, current_sample, period):
     alpha = 2 / float(period + 1)
-    last_sample = current_sample - period
+    last_sample = current_sample - period - 1
     nominator = 0
     denominator = 0
     power = 0
@@ -24,22 +24,22 @@ def main():
     data = pandas.read_csv('wig20_d.csv')['Otwarcie'].values.tolist()
 
     macd = []
-    for sample_number in range(25, len(data)):
+    for sample_number in range(26, len(data)):
         ema12 = ema(data, sample_number, 12)
         ema26 = ema(data, sample_number, 26)
         macd.append(ema12 - ema26)
 
     signal = []
-    for sample_number in range(8, len(macd)):
+    for sample_number in range(9, len(macd)):
         signal.append(ema(macd, sample_number, 9))
 
     fig, (ax1, ax2) = pyplot.subplots(2)
 
-    ax1.plot(data[33:])
+    ax1.plot(data[35:])
     ax1.set_ylabel('Opening stock quotes')
     ax1.set_title('Input data and MACD trading indicator')
 
-    ax2.plot(macd[8:], label='MACD')
+    ax2.plot(macd[9:], label='MACD')
     ax2.plot(signal, label='Signal')
     ax2.set_xlabel('Sample number')
     ax2.set_ylabel('Indicator')

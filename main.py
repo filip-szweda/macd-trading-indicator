8249dd78055275ce241de93a8889842b79c63477
FilipSzweda <filip.szweda1@gmail.com>
2022-03-25 16:11:00 +0100

    Reverted to original function

--- main.py
+++ main.py
@@ -2,20 +2,70 @@ import pandas
 from matplotlib import pyplot
 
 
-def main():
-    data = pandas.read_csv('wig20_d.csv', usecols=['Otwarcie'])  # later change to 'Zamkniecie'
+def eman(data, current_day, period):
+    alpha = 2 / float(period + 1)
+    nominator = 0
+    denominator = 0
+    power = 0
+    last_day = current_day - period - 1
 
-    ema12 = data.ewm(span=12).mean()
-    ema26 = data.ewm(span=26).mean()
-    macd = ema12 - ema26
-    signal = macd.ewm(span=9).mean()
+    for sample in range(current_day, last_day, -1):
+        if type(data) is pandas.DataFrame:
+            value = data.at[sample]
+        else:
+            value = data[sample - 1]
+        denominator += pow(1 - alpha, power)
+        nominator += value * pow(1 - alpha, power)
+        power += 1
+
+    if denominator == 0:
+        return
+
+    return nominator / denominator
+
+
+def ema(data, current_day, period):
+    alpha = 2 / float(period + 1)
+    nominator = 0
+    denominator = 0
+    power = 0
+    last_day = current_day - period - 1
+
+    for sample in range(current_day, last_day, -1):
+        if type(data) is pandas.DataFrame:
+            value = data.at[sample]
+        else:
+            value = data[sample - 1]
+        denominator += pow(1 - alpha, power)
+        nominator += value * pow(1 - alpha, power)
+        power += 1
+
+    if denominator == 0:
+        return
+
+    return nominator / denominator
+
+
+def main():
+    data = pandas.read_csv('wig20_d.csv', usecols=['Otwarcie']).values
+
+    macd = []
+    signal = []
+
+    for sample_number in range(26, 1000):
+        ema12 = ema(data, sample_number, 12)
+        ema26 = ema(data, sample_number, 26)
+        macd.append(ema12 - ema26)
+        if len(macd) >= 9:
+            signal.append(ema(macd, len(macd), 9))
 
     fig, (ax1, ax2) = pyplot.subplots(2)
 
-    ax1.plot(data)
+    ax1.plot(data.tolist()[35:])
     ax1.set_ylabel('Opening exchange')
     ax1.set_title('Input data and MACD trading indicator')
-    ax2.plot(macd, label='MACD')
+
+    ax2.plot(macd[9:], label='MACD')
     ax2.plot(signal, label='Signal')
     ax2.set_xlabel('Sample number')
     ax2.set_ylabel('Indicator')
